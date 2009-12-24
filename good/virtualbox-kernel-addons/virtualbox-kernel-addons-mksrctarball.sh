48661539cff11da77506a4982f33b8ac3e22eb49
pprkut <pprkut@liwjatan.at>
2009-12-24 15:10:00 +0100

    update virtualbox-* for version 3.1.2

---
+++ good/virtualbox-kernel-addons/virtualbox-kernel-addons-mksrctarball.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# This script helps you to make the source package
+# of the virtualbox guest additions kernel modules.
+# IMPORTANT: The virtualbox-ose-addons version you want to use
+# must already be installed!
+
+# Based on the ffmpeg-mksrctarball.sh from the SlackBuilds.org repository
+
+set -e
+
+PRGNAM=virtualbox-kernel-addons
+VERSION=$(VBoxControl --help 2>/dev/null | grep OSE | cut -d " " -f 9 | cut -d "_" -f 1)
+
+mkdir $PRGNAM
+
+echo "--> Copying sourcecode from /usr/src"
+cp -rf /usr/src/vboxguest-$VERSION $PRGNAM/vboxguest
+cp -rf /usr/src/vboxvfs-$VERSION $PRGNAM/vboxvfs
+cp -rf /usr/src/vboxvideo-$VERSION $PRGNAM/vboxvideo
+
+echo "--> Making the sourcecode tarball: $PRGNAM-src-$VERSION.tar.xz"
+tar -cJf $PRGNAM-$VERSION.tar.xz $PRGNAM
+
+echo "--> Erasing the sourcecode directory: $PRGNAM/"
+rm -rf $PRGNAM/
+
+echo "--> Sourcecode tarball for $PRGNAM: $PRGNAM-$VERSION.tar.xz"
