55a20cfefaa7285067b1b142b4ba411a42c30d5b
pprkut <pprkut@liwjatan.at>
2009-12-30 13:39:24 +0100

    restart rc.hald in a chroot

--- good/virtualbox-ose-addons/doinst.sh
+++ good/virtualbox-ose-addons/doinst.sh
@@ -24,5 +24,5 @@ done
 # remove existing fdi cache to recognize newly installed fdi files
 # and restart hal to regenerate the cache
 rm -f var/cache/hald/fdi-cache
-etc/rc.d/rc.hald restart
+chroot . /etc/rc.d/rc.hald restart
 
